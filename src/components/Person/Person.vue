6082b804e43a71d1e84c3860d05ac0473d3e96b3
pinkyQiu <605670628@qq.com>
2018-03-25 19:43:17 +0800

    a

--- src/components/Person/Person.vue
+++ src/components/Person/Person.vue
@@ -10,8 +10,8 @@
         <span class="name">135***2345</span>
     </section>
 
-    <section class="tab_wrapper layout">
-        <ul class="tab_btn">
+    <section class="tab_wrapper layout clearfix">
+        <ul class="tab_btn fl">
             <li 
               v-for="tab in tabs" 
               class="tab_item noselect"
@@ -19,27 +19,29 @@
               @click="switchTab(tab.value)"
             >{{tab.label}}</li>
         </ul>
-        <div class="content">
+        <div class="content fl">
           <div id="order" v-if="currentTab === 'order'">
-            <div class="top clearfix">
-              <div class="left fl">
-                <img src="../images/goodsDepic2.jpg" alt="" class="pic">
+            <div class="order_wrapper" v-for="i in 3">
+              <div class="top clearfix">
+                <div class="left fl">
+                  <img src="../images/goodsDepic2.jpg" alt="" class="pic">
+                </div>
+                <div class="right fl">
+                  <p class="name">ID520-15 I58250 4G 1T+128G MX150-2G 银 高清W10</p>
+                  <p class="type">型号：64寸高清银色</p>
+                  <p class="num">
+                    <span>数量：3</span>
+                    <span>总价：￥1100.00</span>
+                  </p>
+                </div>
               </div>
-              <div class="right fl">
-                <p class="name">ID520-15 I58250 4G 1T+128G MX150-2G 银 高清W10</p>
-                <p class="type">型号：64寸高清银色</p>
-                <p class="num">
-                  <span>数量：3</span>
-                  <span>总价：￥1100.00</span>
+              <div class="btm clearfix">
+                <p class="time fr">下单时间：2018-11-10</p>
+                <p class="cancle fr">
+                  <i class="ico"></i>
+                  <span>取消订单</span>
                 </p>
               </div>
-            </div>
-            <div class="btm">
-              <p class="time">下单时间：2018-11-10</p>
-              <p class="cancle">
-                <i class="ico"></i>
-                <span>取消订单</span>
-              </p>
             </div>
           </div>
           <div id="address" v-if="currentTab === 'address'"></div>
@@ -104,10 +106,11 @@ export default {
   .tab_wrapper{
     margin-bottom: 50px;
     background: #FFFFFF;
-    padding: 30px 20px;
     box-sizing:border-box;
     box-shadow: 1px 1px 3px 0 rgba(0,0,0,0.20);
     .tab_btn{
+      margin: 0 20px;
+      padding-top: 30px;
       border-bottom: 1px solid #DFDFDF;
       font-size: 0;
       .tab_item{
@@ -140,31 +143,52 @@ export default {
       }
     }
   }
-  .content{
-    padding: 20px;
-  }
+  
   #order{
-    .left {
-      width: 120px;
-      text-align: center;
-      .pic{
-        width: 80px;
-        height: 80px;
+    .top{
+      padding: 20px;
+      .left {
+        width: 120px;
+        text-align: center;
+        .pic{
+          width: 80px;
+          height: 80px;
+        }
+      }
+      .right{
+        padding-top: 10px;
+        .name{
+          font-size: 18px;
+          color: #3c3c3c;
+          cursor: pointer;
+          &:hover{
+            color:#f7931e;
+          };
+        }
+        .type{
+          font-size: 16px;
+          color: #9e9e9e;
+          margin: 2px 0;
+        }
+        .num{
+          font-size: 16px;
+          color: #9e9e9e;
+          span{
+            margin-right: 30px;
+          }
+        }
       }
     }
-    .right{
-      padding-top: 10px;
-      .name{
-        font-size: 18px;
-        color: #3c3c3c;
-        cursor: pointer;
-        &:hover{
-          color:#f7931e;
-        };
-      }
-      .type{
-        font-size: 16px;
-        color: #9e9e9e;
+    .btm{
+      margin-top: 20px;
+      border-top:1px solid #ccc; 
+      background-color: #f1f1f1;
+      height: 30px;
+      line-height: 30px;
+      .time{
+        margin-right: 30px;
+        font-size: 14px;
+
       }
     }
   }
